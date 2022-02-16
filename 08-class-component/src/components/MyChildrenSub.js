51e23278133ea88a348c19fe65c91bbd7e59c0d9
NeoJHS <seul5106@gmail.com>
2022-02-16 17:58:51 +0900

    .

---
+++ 08-class-component/src/components/MyChildrenSub.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+class MyChildrenSub extends React.Component {
+    
+
+    /** 화면에 렌러링할 JSX 내용을 리턴하는 함수 */
+    render() {
+        return (
+            <div>
+                <h3>MyChildrenSub</h3>
+
+                
+            </div>
+        );
+    }
+}
+
+export default MyChildrenSub;
